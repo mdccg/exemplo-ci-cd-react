fe8aca21bf53a167fa26cacb16d11203c5101181
Matheus Daniel Cristal Comparotto Gomes <comparotto.js@gmail.com>
2023-06-15 19:49:57 -0400

    Modified cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,6 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
+
+const isProduction = true;
 
 export default defineConfig({
   component: {
@@ -6,13 +8,16 @@ export default defineConfig({
       framework: 'create-react-app',
       bundler: 'webpack',
     },
+
+    video: false,
   },
 
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: isProduction ? 'https://exemplo-ci-cd-react.vercel.app' : 'http://localhost:3000',
+    video: false,
   },
 
   env: {
     API_URL: 'https://api.dictionaryapi.dev/api/v2/entries/en',
   },
-})
+});
